55f8b2c6fe8444a7f585f64f066c4b37590c3a04
Erwan SEITE <wanix.fr@gmail.com>
2014-05-13 23:42:50 +0200

    add new exceptions

--- lib/easyfpm/exceptions.rb
+++ lib/easyfpm/exceptions.rb
@@ -2,3 +2,5 @@
 
 #Raised if an unknown changelog format is asked
 class EASYFPM::InvalidChangelogFormat < StandardError; end
+class EASYFPM::MissingArgument < StandardError; end
+class EASYFPM::NoSuchSection < StandardError; end
