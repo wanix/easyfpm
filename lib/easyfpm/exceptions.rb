c4e6333e77d9614b1f3c2dea0970b283d9e83fff
wanix <wanix.fr@gmail.com>
2014-05-14 19:03:17 +0200

    add exceptions

--- lib/easyfpm/exceptions.rb
+++ lib/easyfpm/exceptions.rb
@@ -6,3 +6,11 @@ class EASYFPM::InvalidChangelogFormat < StandardError; end
 class EASYFPM::MissingArgument < StandardError; end
 #Raised if a packaging label is asked and not configured
 class EASYFPM::NoSuchSection < StandardError; end
+#Raised if a loop is detected with templates
+class EASYFPM::LoopTemplateDetected < StandardError; end
+#Raised if a template var is asked and not found
+class EASYFPM::NoTemplateFound < StandardError; end
+#Raised if a template var is not a string
+class EASYFPM::InvalidTemplateType < StandardError; end
+#Raised if a configuration is not validated
+class EASYFPM::InvalidConfiguration < StandardError; end
