2610d6bf220e85ce88c82ed5642cb33db1957e2c
Erwan SEITE <wanix.fr@gmail.com>
2014-05-14 23:10:36 +0200

    continue work

--- lib/easyfpm/packaging.rb
+++ lib/easyfpm/packaging.rb
@@ -8,6 +8,7 @@
 require "easyfpm"
 require "unixconfigstyle"
 require "tempfile"
+require "ptools"
 
 class EASYFPM::Packaging
 
@@ -17,10 +18,22 @@ class EASYFPM::Packaging
   #Initialize the class
   def initialize(unixconfigstyle, specificLabel=nil)
     raise ArgumentError, 'the argument must be an UnixConfigStyle object' unless unixconfigstyle.is_a? UnixConfigStyle
+
+    #We need fpm in the path
+    raise EASYFPM::InvalidEnvironment, "the fpm script is not found in PATH, please install it or put it in your PATH" unless File.which("fpm")
+
     @easyfpmconf = EASYFPM::Configuration.new(unixconfigstyle, specificLabel)
+    @label=specificLabel
     self.verbose = false
     self.dryrun = false
   end #initialize
+
+  #return a fpmcmdline for the specific label
+  def generatefpmline(labelhashconf)
+    return nil unless labelhashconf
+    fpmconf=""
+    return fpmconf
+  end #generatefpmline
 
   #Create the packages
   def make()
