c2c0a75f4261151c0cb3bbcbd37432a404136cd2
wanix <wanix.fr@gmail.com>
2014-05-14 12:32:50 +0200

    add comments

--- lib/easyfpm/exceptions.rb
+++ lib/easyfpm/exceptions.rb
@@ -2,5 +2,7 @@
 
 #Raised if an unknown changelog format is asked
 class EASYFPM::InvalidChangelogFormat < StandardError; end
+#Raised if a needed argument for a package is missing
 class EASYFPM::MissingArgument < StandardError; end
+#Raised if a packaging label is asked and not configured
 class EASYFPM::NoSuchSection < StandardError; end
